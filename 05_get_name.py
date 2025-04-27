9491c135ebdb72826076334dbb0f546784429e09
MehraniAI <ddmehranifd@gmail.com>
2025-04-27 09:48:06 +0500

    06_functions

---
+++ 05_get_name.py
@@ -0,0 +1,19 @@
+def main():
+    print("Delete this line and write your code here! :)")
+
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
+    def get_name():
+     "Sophia"
+
+# There is no need to edit code beyond this point
+
+def main():
+    name = get_name() # get_name() will return a string which we store to the 'name' variable here
+    print("Howdy", name, "! 🤠")
+
+if __name__ == '__main__':
+    main()
---
+++ 10_print_ones_digit.py
@@ -0,0 +1,13 @@
+def main():
+    print("Delete this line and write your code here! :)")
+
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
+    def print_ones_digit(num): print("The ones digit is", num % 10)
+
+def main():
+    num = int(input("Enter a number: "))    
+    print_ones_digit(num)
